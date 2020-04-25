57a86d2e41e9ff1515f7d7c3e7b9934b54f731ba
Projax <projrx@hamzapervaiz.com>
2020-04-26 02:33:57 +0500

    initalize

---
+++ include/header.php
@@ -0,0 +1,197 @@
+	<!-- Top Bar
+		============================================= -->
+		<div id="top-bar">
+
+			<div class="container clearfix">
+
+				<div class="col_half nobottommargin d-none d-md-block">
+
+					<p class="nobottommargin"><strong>Call:</strong> 1800-547-2145 | <strong>Email:</strong> info@hiphop.pk</p>
+
+				</div>
+
+				<div class="col_half col_last fright nobottommargin">
+
+					<!-- Top Links
+					============================================= -->
+					<div class="top-links">
+						<ul>
+
+							<li>
+								<div id="top-cart" style="margin-top: 12px;">
+							<a href="#" id="top-cart-trigger" style="width: 100px"> View Cart<i class="icon-shopping-cart"></i><span>5</span></a>
+							<div style="    width: 350px;    z-index: 9999;" class="top-cart-content">
+								<div class="top-cart-title">
+									<h4>Shopping Cart</h4>
+								</div>
+								<div class="top-cart-items">
+									<div class="top-cart-item clearfix">
+										<div class="top-cart-item-image">
+											<a href="#"><img src="images/products/p3.jpg" alt="Blue Round-Neck Tshirt" /></a>
+										</div>
+										<div class="top-cart-item-desc">
+											<a href="#">Blue Round-Neck Tshirt</a>
+											<span class="top-cart-item-price">$19.99</span>
+											<span class="top-cart-item-quantity">x 2</span>
+										</div>
+									</div>
+									<div class="top-cart-item clearfix">
+										<div class="top-cart-item-image">
+											<a href="#"><img src="images/products/p2.jpg" alt="Light Blue Denim Dress" /></a>
+										</div>
+										<div class="top-cart-item-desc">
+											<a href="#">Light Blue Denim Dress</a>
+											<span class="top-cart-item-price">$24.99</span>
+											<span class="top-cart-item-quantity">x 3</span>
+										</div>
+									</div>
+								</div>
+								<div class="top-cart-action clearfix">
+									<span class="fleft top-checkout-price">$114.95</span>
+									<button class="button button-3d button-small nomargin fright">View Cart</button>
+								</div>
+							</div>
+						</div><!-- #top-cart end -->
+							</li>
+						</ul>
+					</div><!-- .top-links end -->
+
+				</div>
+
+			</div>
+
+		</div><!-- #top-bar end -->
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="sticky-style-2">
+
+			<div class="container clearfix">
+
+				<!-- Logo
+				============================================= -->
+				<div id="logo">
+					<a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+					<a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+				</div><!-- #logo end -->
+
+				<ul class="header-extras">
+					<li>
+						<i style="" class="i-medium i-circled i-bordered icon-thumbs-up2 nomargin bodgreen colgreen"></i>
+						<div class="he-text">
+							Original Brands
+							<span>100% Guaranteed</span>
+						</div>
+					</li>
+					<li>
+						<i  style="" class="i-medium i-circled i-bordered icon-truck2 nomargin colred bodred"></i>
+						<div class="he-text">
+							Free Shipping
+							<span>for $20 or more</span>
+						</div>
+					</li>
+					<li>
+						<i  class="i-medium i-circled i-bordered icon-undo nomargin colblue bodblue"></i>
+						<div class="he-text">
+							30-Day Returns
+							<span>Completely Free</span>
+						</div>
+					</li>
+				</ul>
+
+			</div>
+
+			<div id="header-wrap">
+
+				<!-- Primary Navigation
+				============================================= -->
+				<nav id="primary-menu" class="style-2">
+
+					<div class="container clearfix">
+
+						<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+							<ul>
+								
+								<li><a href="#"><div>Mini Boy</div><span>Out of the Box</span></a>
+								<div class="mega-menu-content style-2 clearfix">
+									<ul class="mega-menu-column col-lg-6">
+										<li class="mega-menu-title"><a href="#"><div> <i class="icon-thumbs-up21"></i> Special</div></a>
+											<ul>
+												<li><a href="#"><div>Sales</div></a></li>
+												<li><a href="#"><div>New Arrivals</div></a></li>
+												<li><a href="#"><div>Featured</div></a></li>
+											</ul>
+										</li>
+									</ul>
+									<ul class="mega-menu-column col-lg-6">
+										<li class="mega-menu-title"><a href="#"><div><i class="icon-child"></i> Child Clothing</div></a>
+											<ul>
+												<li><a href="#"><div>Casual Shirts</div></a></li>
+												<li><a href="#"><div>T-Shirts</div></a></li>
+												<li><a href="#"><div>Collared Tees</div></a></li>
+												<li><a href="#"><div>Pants / Trousers</div></a></li>
+												<li><a href="#"><div>Ethnic Wear</div></a></li>
+												<li><a href="#"><div>Jeans</div></a></li>
+												<li><a href="#"><div>Sweamwear</div></a></li>
+											</ul>
+										</li>
+									</ul>
+								</div>
+							</li><!-- .mega-menu end -->
+
+							</ul>
+
+						<!-- Top Cart
+						============================================= -->
+						<div id="top-cart">
+							<a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>5</span></a>
+							<div class="top-cart-content">
+								<div class="top-cart-title">
+									<h4>Shopping Cart</h4>
+								</div>
+								<div class="top-cart-items">
+									<div class="top-cart-item clearfix">
+										<div class="top-cart-item-image">
+											<a href="#"><img src="images/products/p3.jpg" alt="Blue Round-Neck Tshirt" /></a>
+										</div>
+										<div class="top-cart-item-desc">
+											<a href="#">Blue Round-Neck Tshirt</a>
+											<span class="top-cart-item-price">$19.99</span>
+											<span class="top-cart-item-quantity">x 2</span>
+										</div>
+									</div>
+									<div class="top-cart-item clearfix">
+										<div class="top-cart-item-image">
+											<a href="#"><img src="images/products/p2.jpg" alt="Light Blue Denim Dress" /></a>
+										</div>
+										<div class="top-cart-item-desc">
+											<a href="#">Light Blue Denim Dress</a>
+											<span class="top-cart-item-price">$24.99</span>
+											<span class="top-cart-item-quantity">x 3</span>
+										</div>
+									</div>
+								</div>
+								<div class="top-cart-action clearfix">
+									<span class="fleft top-checkout-price">$114.95</span>
+									<button class="button button-3d button-small nomargin fright">View Cart</button>
+								</div>
+							</div>
+						</div><!-- #top-cart end -->
+
+						<!-- Top Search
+						============================================= -->
+						<div id="top-search">
+							<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+							<form action="search.html" method="get">
+								<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+							</form>
+						</div><!-- #top-search end -->
+
+					</div>
+
+				</nav><!-- #primary-menu end -->
+
+			</div>
+
+		</header><!-- #header end -->
